65d145c32a09fdbed3481e95c94890abedbfbf4f
student <student@10.2.3.1>
2016-04-04 15:13:32 +0200

    Quick_find - commit gałęzi

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -12,10 +12,17 @@ public class UF {
 	}
 	
 	public void union(int p, int q) {
+		 int pid = id[p];
+		 int qid = id[q];
+		 for (int i = 0; i < id.length; ++i) {
+			 if (id[i] == pid) {
+				 	id[i] = qid;
+			 }
+		 }
 	}
 	
 	public boolean connected(int p, int q) {
-		return false;
+		 return id[p] == id[q];
 	}
 	
 }
